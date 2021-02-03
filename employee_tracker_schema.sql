77fdf1a4f6e9a7eddc89a63df64a3d1d7a48fe88
David Rojo <rojo@rojorevolution.com>
2021-02-03 17:49:54 -0600

    updated schema, moving to branch to begin working with JS

--- employee_tracker_schema.sql
+++ employee_tracker_schema.sql
@@ -3,24 +3,26 @@ CREATE database employees_db;
 USE employees_db;
 
 CREATE TABLE department(
-  id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(30) NULL,
-  PRIMARY KEY (id)
+  id INT NOT NULL AUTO_INCREMENT primary key,
+  name VARCHAR(30) NULL
 );
 
 CREATE TABLE role(
-  id INT NOT NULL AUTO_INCREMENT,
+  id INT NOT NULL AUTO_INCREMENT primary key,
   title VARCHAR(30) NULL,
   salary DECIMAL(8,2),
-  PRIMARY KEY (id)
+  department_id INT,
+  FOREIGN KEY (department_id)
+	REFERENCES department(id)
+	ON DELETE CASCADE
 );
 
-
 CREATE TABLE employee(
-    id INT NOT NULL auto_increment,
+    id INT NOT NULL auto_increment primary key,
     first_name VARCHAR(30) NULL,
     last_name VARCHAR(30) NULL,
     role_id INT,
-    manager_id INT,
-    primary key (id)
+    FOREIGN KEY (role_id)
+		REFERENCES role(id),
+    manager_id INT NULL
 );
